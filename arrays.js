55a76de670b32c6e9fd33a42cc097018acfca14e
Jes Albro <jesalbro@gmail.com>
2017-11-12 19:51:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var a = [element, ...array];
+  var array = [element, ...array];
   return a;
 }
